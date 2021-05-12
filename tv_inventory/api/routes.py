5661cec214e62d74be5060c49f57892bee6eba60
ksillah <kutah.sillah@gmail.com>
2021-05-11 21:33:35 -0500

    can't create

---
+++ tv_inventory/api/routes.py
@@ -0,0 +1,79 @@
+from flask import Blueprint, request, jsonify
+from tv_inventory.helpers import token_required
+from tv_inventory.models import User, Review, tv_schema, tv_schemas, db
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return {'some_value': 52, 'another_value': 73}
+
+#create review endpoint
+@api.route('/reviews', methods = ['POST'])
+@token_required
+def create_review(current_user_token): #coming from token_required decorator
+    show = request.json['show']
+    season = request.json['season']
+    episode= request.json['episode']
+    rating = request.json['rating']
+    user_token = current_user_token.token
+   
+    review = Review(show, season, episode, rating,user_token=user_token)
+    db.session.add(review)
+    db.session.commit()
+    response = tv_schema.dump(review)
+    return jsonify(response)
+
+#retrieve all reviews
+@api.route('/reviews', methods = ['GET'])
+@token_required
+def get_reviews(current_user_token):
+    owner = current_user_token.token
+    reviews = Review.query.filter_by(user_token = owner).all()
+    response = tv_schemas.dump(reviews)
+    return jsonify(response)
+
+#retrieve one  endpoint
+@api.route('/reviews/<id>', methods = ['GET'])
+@token_required
+def get_review(current_user_token, id):
+    review = Review.query.get(id)
+    response = tv_schema.dump(review)
+    return jsonify(response)
+
+#update review by id
+@api.route('/reviewss/<id>', methods = ['POST', 'PUT'])
+@token_required
+def update_review(current_user_token, id):
+    review = Review.query.get(id)
+    #different notation from instantiation
+    review.show = request.json['show']
+    review.season = request.json['season']
+    review.episode = request.json['episode']
+    review.rating = request.json['rating']
+    review.user_token = current_user_token.token
+
+    db.session.commit()
+    response = tv_schema.dump(review)
+    return jsonify(response)
+
+#delete review by ID
+@api.route('/reviews/<id>', methods = ['DELETE'])
+@token_required
+def delete_review(current_user_token,id):
+    review=Review.query.get(id)
+    db.session.delete(review)
+    db.session.commit()
+    response=tv_schema.dump(review)
+    return jsonify(response)
+
+
+
+    
+
+
+
+
+
+
+
